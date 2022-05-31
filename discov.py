ce2b6fcd3a33fb65fd5875a97d430b026d94fb6e
Defeat3d <koen.deubel@hva.nl>
2022-06-01 00:47:39 +0200

    Initial commit

---
+++ discov.py
@@ -0,0 +1,106 @@
+import disnake
+import json
+import sys
+import random
+import time
+
+args = sys.argv[1:]
+token = args[0]
+file = args[1]
+
+with open(file, encoding='utf-8') as input_file:
+    markovs = json.load(input_file)
+    print(f"Discov is keeping track of {len(markovs)} users")
+    input_file.close()
+
+
+def add_phrase(markov, text: str, min_words: int = 4):
+    words = text.split(' ')
+    if len(words) < min_words:
+        print(f"Phrase has less words than the minimum of {min_words}")
+        return
+
+    for index, word in enumerate(words):
+        if index == 0:
+            if "_start" not in markov:
+                markov["_start"] = []
+            markov["_start"].append(word)
+
+            if index != len(words) - 1:
+                if word not in markov:
+                    markov[word] = []
+                markov[word].append(words[index + 1])
+
+        elif index == len(words) - 1:
+            if "_end" not in markov:
+                markov["_end"] = []
+            markov["_end"].append(word)
+
+        else:
+            if word not in markov:
+                markov[word] = []
+            markov[word].append(words[index + 1])
+
+
+def generate(markov):
+    phrase = []
+    starts = markov["_start"]
+    if not starts:
+        return None
+    word = random.choice(starts)
+    phrase.append(word)
+
+    while word and not word.endswith(('.', '?', '!')):
+        if word not in markov:
+            return None
+        word = random.choice(markov[word])
+        phrase.append(word)
+
+    result = ' '.join(phrase)
+    print(f"Generated Markov chain: {result}")
+    return result
+
+
+class DiscovClient(disnake.Client):
+    def __init__(self, **options):
+        super().__init__(**options)
+        self.last_save = 0
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user} ({self.user.id})")
+
+    async def on_message(self, message):
+        author_id = str(message.author.id)
+        self_id = str(self.user.id)
+        content = message.content
+        if not content.startswith("!markov"):
+            if author_id != self_id:
+                if author_id not in markovs:
+                    markovs[author_id] = dict()
+                if self_id not in markovs:
+                    markovs[self_id] = dict()
+                formatted = content.capitalize() if content.endswith(('.', '?', '!')) else f"{content.capitalize()}."
+                print(f"Adding phrase \"{formatted}\" to user {author_id}")
+                add_phrase(markovs[author_id], formatted)
+                print(f"Adding phrase \"{formatted}\" to user {self_id}")
+                add_phrase(markovs[self_id], formatted)
+
+                if time.time() - self.last_save > 900:
+                    with open(file, 'w', encoding='utf-8') as output_file:
+                        json.dump(markovs, output_file)
+                        self.last_save = time.time()
+                        output_file.close()
+        elif author_id != self_id:
+            if content.startswith("!markov purge"):
+                del markovs[author_id]
+            user_id = str(message.mentions[0].id) if message.mentions else self_id
+            if user_id in markovs:
+                markov = markovs[user_id]
+                if markov:
+                    generated = generate(markov)
+                    if generated:
+                        await message.channel.send(generated)
+
+
+client = DiscovClient(intents=disnake.Intents.all())
+client.run(token)
